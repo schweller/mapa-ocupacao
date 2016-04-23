b4e3fc72771c21d33604985fafa07015fdec4ae6
Inácio Schweller <iscmendonca@gmail.com>
2016-04-23 16:56:41 -0300

    adding the map

--- index.js
+++ index.js
@@ -2,14 +2,42 @@ var MAPS_KEY="AIzaSyB_x2KmeI9S3tE7JMf6-k_SabWMiA74nus";
 
 (function() {
 
-  var map;
-  function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
-    });
-  }
+  var ocupa = (function () {
+    var instance;
 
-  initMap()
+    function init() {
+      var map;
+
+      return {
+        initMap: function () {
+          map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 8
+          });
+        },
+
+        getMap: function () {
+            if ( !map ) {
+              initMap()
+            }
+            return map
+        }
+
+      };
+    };
+
+    return {
+      getInstance: function () {
+        if ( !instance ) {
+          instance = init();
+        }
+        return instance;
+      }
+    };
+
+  })();
+
+  var mapa = ocupa.getInstance()
+  mapa.initMap()
 
 })()
